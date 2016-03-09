2447c7e677e474ebfb865bf6137cdf6c2c142ed4
Hagin Onyango <hagincubes@gmail.com>
2016-03-09 14:01:20 -0500

    Updated from Brython Server: March 9, 2016 2:01:21 PM EST

--- stringjumble.py
+++ stringjumble.py
@@ -25,7 +25,7 @@ ydnah dnif yam uoy taht skcirt ro seuqinhcet wef a era erehT
 handy find may you that tricks or techniques few a are There
 erehT era a wef seuqinhcet ro skcirt taht uoy yam dnif ydnah
 """
-Input = input("Enter a sentence, a word or a paragraph, the more the better ;): ")
+Input = input("Please enter a string of text (the bigger the better): ")
 
 print('You entered "'+Input+'". Now jumble it: ')
 print(Input[:: -1])
