eb770f99c94fef3415e614eab0caa9696d37b7b5
Hagin Onyango <hagincubes@gmail.com>
2016-03-09 13:45:27 -0500

    Updated from Brython Server: March 9, 2016 1:45:27 PM EST

--- stringjumble.py
+++ stringjumble.py
@@ -25,4 +25,15 @@ ydnah dnif yam uoy taht skcirt ro seuqinhcet wef a era erehT
 handy find may you that tricks or techniques few a are There
 erehT era a wef seuqinhcet ro skcirt taht uoy yam dnif ydnah
 """
-Input = input("Enter a sentence, a word or paragraph ")
+Input = input("Enter a sentence, a word or a paragraph, the more the better ;): ")
+
+print('You entered "'+Input+'". Now jumble it: ')
+print(Input[:: -1])
+
+x = Input.split()
+
+Print = 1 
+
+while Print< len(x):
+    print(x[-Print],end=" ")
+    Print +=
